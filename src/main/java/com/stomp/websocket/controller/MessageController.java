4268006eb0b4d61744d40cf3fdfc2ebf0d9b7b10
Marco Seabra <marcogutto@gmail.com>
2019-11-01 15:01:23 -0300

    Adicionando configuração do backend e do frontend

---
+++ src/main/java/com/stomp/websocket/controller/MessageController.java
@@ -0,0 +1,24 @@
+package com.stomp.websocket.controller;
+
+import java.time.LocalDateTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import domain.Message;
+
+@Controller
+public class MessageController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageController.class);
+
+    @MessageMapping("/send")
+    @SendTo("/topic/messages")
+    public Message send(Message message) {
+        LOGGER.info(String.format("Received message [%s] on `/app/chat` message mapping!", message.toString()));
+        LocalDateTime timestamp = LocalDateTime.now();
+        return new Message(message.getFrom(), message.getMessage(), timestamp);
+    }
+}
